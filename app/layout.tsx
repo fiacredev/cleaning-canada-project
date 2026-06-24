09ac87fc3fbf280985ce84e58d77b9d41f5912d8
fiacredev <fiacredeveloper@example.com>
2026-06-24 19:23:38 +0200

    just added google search console codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,18 @@ export const metadata: Metadata = {
   title: "Professional Cleaning & Snow Removal Services | HubertGreenefy",
   description:
     "Professional cleaning services for homes, offices, and commercial facilities.",
+    verification:{
+      google:"wNTtNestFDJnRzvv0jWlaY9sfvnmGKKPq6w_DUH0GsI"
+    },
+    openGraph: {
+    title: "HubertGreenefy",
+    description:
+      "Professional cleaning services",
+    images: ["/hero.png"],
+  }
 };
+
+
 
 export default function RootLayout({
   children,
